454947dbca0c5e0a2da25b9d5d1bac2888b24b0c
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-01-29 19:12:14 +0330

    start design homepage

--- content/view.php
+++ content/view.php
@@ -6,12 +6,10 @@ class view
 	public static function config()
 	{
 		// define default value for global
-		\dash\data::site_title(T_("Dash"));
-		\dash\data::site_desc(T_("Free PHP Framework & CMS!"));
-		\dash\data::site_slogan(T_("The simple framework for php programmers ;)"));
+		\dash\data::site_title(T_("Salam Quran"));
+		\dash\data::site_desc(T_("Say hello to Quran!"). ' '. T_("Quran is calling you."). ' '. T_("Access to Quran in your language."). ' '. T_("Free and Easy."));
+		\dash\data::site_slogan(T_("Quran Anywhere Anytime"));
 		\dash\data::page_desc(\dash\data::site_desc(). ' | '. \dash\data::site_slogan());
-		\dash\data::include_css(false);
-		\dash\data::bodyclass(null);
 	}
 }
 ?>
