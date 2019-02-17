a9e8af1d4d0a945c2f4628547b0322a5a4efe5d4
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-17 18:53:17 +0330

    add footer

--- content/view.php
+++ content/view.php
@@ -10,6 +10,14 @@ class view
 		\dash\data::site_desc(T_("Say hello to Quran!"). ' '. T_("Quran is calling you."). ' '. T_("Access to Quran in your language."). ' '. T_("Free and Easy."));
 		\dash\data::site_slogan(T_("Quran Anywhere Anytime"));
 		\dash\data::page_desc(\dash\data::site_desc(). ' | '. \dash\data::site_slogan());
+
+		\dash\data::page_copyright(
+			T_('© :year :site. All right reserved.',
+			[
+				'year' => \dash\datetime::fit("now", "Y"),
+				'site' => "<a href='". \dash\url::kingdom(). "' title='". \dash\data::site_desc() ."'>". \dash\data::site_title(). "</a>"
+			]
+		));
 	}
 }
 ?>
