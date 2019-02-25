cf5456678c2698fea26f9461499f287d3bd049ef
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-25 13:26:19 +0330

    Automatic update Siftal! 0_0

---
+++ content/namaz/view.php
@@ -0,0 +1,12 @@
+<?php
+namespace content\namaz;
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_("Hasten to the prayer"));
+		\dash\data::page_desc(T_("Muslim Prayer Times"));
+	}
+}
+?>
