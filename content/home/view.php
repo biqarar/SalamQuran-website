c14cbdf271baeb485c9d886cdc84b30ba1f7dd89
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-25 19:33:15 +0330

    set title of homepage

--- content/home/view.php
+++ content/home/view.php
@@ -5,8 +5,9 @@ class view
 {
 	public static function config()
 	{
-		\dash\data::page_title(T_("Quran"));
+		\dash\data::page_title(T_("Salam Quran"));
 		\dash\data::page_desc(T_("Say hello to Quran!"). ' '. T_("Quran is calling you."));
+		\dash\data::page_special(true);
 
 	}
 }
