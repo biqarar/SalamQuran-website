4f9a0ec939d5c2064326350081f21ba5cdc691d8
Reza <rm.biqarar@gmail.com>
2019-02-27 11:47:05 +0330

    quick load sure to fix later

--- content/home/controller.php
+++ content/home/controller.php
@@ -6,16 +6,15 @@ class controller
 	public static function routing()
 	{
 		$module = \dash\url::module();
-		if(ctype_digit($module))
+
+		$module = intval(str_replace('s', '', $module));
+		if($module >= 1 && $module <= 114)
 		{
-			$module = intval($module);
-			if($module >= 1 && $module <= 114)
-			{
-				$load_sura = \lib\app\sura::load($module);
-				\dash\data::sura($load_sura);
-				\dash\open::get();
-			}
+			$load_sura = \lib\app\sura::load($module);
+			\dash\data::sura($load_sura);
+			\dash\open::get();
 		}
+
 	}
 }
 ?>
