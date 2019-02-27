73f6b02cd5b447fad60d4ed006d35d3f46f2bf08
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-27 18:10:04 +0330

    update style

--- content/contact/view.php
+++ content/contact/view.php
@@ -7,7 +7,7 @@ class view extends \content_support\ticket\contact_ticket\view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Contact Us'));
-		\dash\data::page_desc(T_("Knowing your valuable comments about bugs and problems and more importantly your precious offers will help us in this way."));
+		\dash\data::page_desc(T_("Help us improve SalamQuran project"));
 		self::codeurl();
 	}
 }
