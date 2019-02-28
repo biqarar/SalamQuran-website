a9284ed7fc2f4183752f65783c43194b44c024d1
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-28 18:15:17 +0330

    add logo page

---
+++ content/logo/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\logo;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('SalamQuran Logo'));
+		\dash\data::page_desc(T_('Need know more about SalamQuran Logo? We are not choose our final logo yet!'));
+	}
+}
+?>
