ac85e78ad7210806457c2a59f77f8cfebf33976a
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-28 18:14:06 +0330

    add terms page

---
+++ content/terms/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\terms;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('Terms of Service Agreement'));
+		\dash\data::page_desc(T_('SalamQuran acts upon international rules, depends on the countries receiving its services and renders its activities within this framework.'));
+	}
+}
+?>
