d8b71ad90b03a2fdfd6e77afc7d2c33f79e0ceed
Reza <rm.biqarar@gmail.com>
2019-03-05 16:19:31 +0330

    sort quran

--- includes/lib/db/quran.php
+++ includes/lib/db/quran.php
@@ -7,6 +7,7 @@ class quran
 	public static function get($_where, $_option = [])
 	{
 		$_option['db_name'] = \lib\db\db_data_name::get();
+		$_option['order'] = ' ORDER BY `1_quran_ayat`.`index` ASC ';
 		return \dash\db\config::public_get('1_quran_ayat', $_where, $_option);
 	}
 
--- includes/lib/db/quran_word.php
+++ includes/lib/db/quran_word.php
@@ -7,6 +7,7 @@ class quran_word
 	public static function get($_where, $_option = [])
 	{
 		$_option['db_name'] = \lib\db\db_data_name::get();
+		$_option['order'] = ' ORDER BY `1_quran_word`.`id` ASC ';
 		return \dash\db\config::public_get('1_quran_word', $_where, $_option);
 	}
 
