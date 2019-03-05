d4ea2ec5b0b014cf86402925ee48a59a3e64c1ff
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-03-05 17:45:06 +0330

    update about

--- content/about/view.php
+++ content/about/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('About Salam Quran'));
-		\dash\data::page_desc(\dash\data::site_desc());
+		\dash\data::page_desc(T_('Free Online Project to Share Quran for you'));
 	}
 }
 ?>
