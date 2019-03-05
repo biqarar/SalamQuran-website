af96511180e29280d90251e1fe1a1b79b52095ee
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-03-05 18:04:42 +0330

    add vision

--- content/vision/view.php
+++ content/vision/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Salam Quran Vision'));
-		\dash\data::page_desc(\dash\data::site_desc());
+		\dash\data::page_desc(T_('Covering all Quranic information of Muslims'));
 	}
 }
 ?>
