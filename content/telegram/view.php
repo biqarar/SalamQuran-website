ac58d736505539569bb202223c74977fc0a06be8
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-03-06 19:13:59 +0330

    add telegram page

---
+++ content/telegram/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\telegram;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('SalamQuran Telegram Bot'));
+		\dash\data::page_desc(T_('Soon'));
+	}
+}
+?>
