78fda5e12d636aee0e461294d7ef1f60f7c0f4fa
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-03-06 17:51:49 +0330

    add apps

---
+++ content/apps/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\apps;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('SalamQuran Apps'));
+		\dash\data::page_desc(T_('Soon'));
+	}
+}
+?>
