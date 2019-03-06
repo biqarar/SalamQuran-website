012a4a93b14cef3d93acd4a6e1d2c97aa371f8e1
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-03-06 19:13:44 +0330

    add developer page

---
+++ content/developers/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\developers;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('SalamQuran Developers'));
+		\dash\data::page_desc(T_('Soon'));
+	}
+}
+?>
