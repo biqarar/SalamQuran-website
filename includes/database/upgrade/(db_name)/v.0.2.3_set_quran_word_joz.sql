bcd44cf746144659470677a73af495062a480717
Reza <rm.biqarar@gmail.com>
2019-03-07 19:49:23 +0330

    add juz, hizb and rub to quran_word

---
+++ includes/database/upgrade/(db_name)/v.0.2.3_set_quran_word_joz.sql
@@ -0,0 +1,5 @@
+UPDATE `salamquran_data`.`1_quran_word` SET `salamquran_data`.`1_quran_word`.`juz` = ( SELECT `salamquran_data`.`1_quran_ayat`.`juz` FROM `salamquran_data`.`1_quran_ayat` WHERE `salamquran_data`.`1_quran_ayat`.`sura` = `salamquran_data`.`1_quran_word`.`sura` AND `salamquran_data`.`1_quran_ayat`.`aya` = `salamquran_data`.`1_quran_word`.`aya` LIMIT 1 );
+
+UPDATE `salamquran_data`.`1_quran_word` SET `salamquran_data`.`1_quran_word`.`hizb` = ( SELECT `salamquran_data`.`1_quran_ayat`.`hizb` FROM `salamquran_data`.`1_quran_ayat` WHERE `salamquran_data`.`1_quran_ayat`.`sura` = `salamquran_data`.`1_quran_word`.`sura` AND `salamquran_data`.`1_quran_ayat`.`aya` = `salamquran_data`.`1_quran_word`.`aya` LIMIT 1 );
+
+UPDATE `salamquran_data`.`1_quran_word` SET `salamquran_data`.`1_quran_word`.`rub` = ( SELECT `salamquran_data`.`1_quran_ayat`.`rub` FROM `salamquran_data`.`1_quran_ayat` WHERE `salamquran_data`.`1_quran_ayat`.`sura` = `salamquran_data`.`1_quran_word`.`sura` AND `salamquran_data`.`1_quran_ayat`.`aya` = `salamquran_data`.`1_quran_word`.`aya` LIMIT 1 );
