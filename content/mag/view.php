8e6db7f23cd60e6189d416f165aa6e52ebc1ecde
Reza <rm.biqarar@gmail.com>
2019-03-07 18:45:05 +0330

    add sample mag page to fix next time and have not 404

---
+++ content/mag/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\mag;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('SalamQuran mag'));
+		\dash\data::page_desc(T_('Need know more about SalamQuran mag? We are not choose our final logo yet!'));
+	}
+}
+?>
