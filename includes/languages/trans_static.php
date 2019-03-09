e5d521ccc62d2d3e1da56948b5bd8872a06efc32
Reza <rm.biqarar@gmail.com>
2019-03-09 19:36:39 +0330

    add static trans for language name

--- includes/languages/trans_static.php
+++ includes/languages/trans_static.php
@@ -11,6 +11,50 @@ class TransStatic
 	echo T_("meccan");
 	echo T_("Meccan");
 
+	echo T_("Amharic");
+	echo T_("Arabic");
+	echo T_("Azerbaijani");
+	echo T_("Amazigh");
+	echo T_("Bulgarian");
+	echo T_("Bengali");
+	echo T_("Bosnian");
+	echo T_("Czech");
+	echo T_("German");
+	echo T_("Divehi");
+	echo T_("English");
+	echo T_("Spanish");
+	echo T_("Persian");
+	echo T_("French");
+	echo T_("Hausa");
+	echo T_("Hindi");
+	echo T_("Indonesian");
+	echo T_("Italian");
+	echo T_("Japanese");
+	echo T_("Korean");
+	echo T_("Kurdish");
+	echo T_("Malayalam");
+	echo T_("Malay");
+	echo T_("Dutch");
+	echo T_("Norwegian");
+	echo T_("Polish");
+	echo T_("Pashto");
+	echo T_("Portuguese");
+	echo T_("Romanian");
+	echo T_("Russian");
+	echo T_("Sindhi");
+	echo T_("Somali");
+	echo T_("Albanian");
+	echo T_("Swedish");
+	echo T_("Swahili");
+	echo T_("Tamil");
+	echo T_("Tajik");
+	echo T_("Thai");
+	echo T_("Turkish");
+	echo T_("Tatar");
+	echo T_("Uyghur");
+	echo T_("Urdu");
+	echo T_("Uzbek");
+	echo T_("Chinese");
 
 	}
 }
