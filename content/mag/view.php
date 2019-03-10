2d4dd4459cd51ae94b452795bc2b9524df03385a
Reza <rm.biqarar@gmail.com>
2019-03-10 09:59:41 +0330

    update trans

--- content/mag/view.php
+++ content/mag/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('SalamQuran mag'));
-		\dash\data::page_desc(T_('Need know more about SalamQuran mag? We are not choose our final logo yet!'));
+		\dash\data::page_desc(T_('Need know more about SalamQuran mag?'));
 	}
 }
 ?>
