a5759cfbfeab1b0a9638bdcfa88f5cdf7854f3b5
Reza <rm.biqarar@gmail.com>
2019-03-11 10:01:21 +0330

    fix image of qari

--- includes/lib/app/qari.php
+++ includes/lib/app/qari.php
@@ -17,7 +17,7 @@ class qari
 
 	private static function master_path()
 	{
-		return 'https://dl.salamquran.com/ayat/';
+		return 'https://dl.salamquran.com/';
 	}
 
 	public static function list()
@@ -25,48 +25,48 @@ class qari
 		$list =
 		[
 			// ----------------- abdoabaset
-			['index' => 1000, 'lang' => 'ar', 'type' => T_('Mujawwad'), 'addr'  => 'abdulbasit-mujawwad-128/', 'slug'  => 'abdulbaset', 'name'  => T_('AbdulBaset AbdulSamad'), 'default' => true],
-			['index' => 1001, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'abdulbasit-murattal-192/', 'slug'  => 'abdulbaset', 'name'  => T_('AbdulBaset AbdulSamad'),],
+			['index' => 1000, 'lang' => 'ar', 'type' => T_('Mujawwad'), 'addr'  => 'ayat/abdulbasit-mujawwad-128/', 'slug'  => 'abdulbaset', 'name'  => T_('AbdulBaset AbdulSamad'), 'default' => true],
+			['index' => 1001, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'ayat/abdulbasit-murattal-192/', 'slug'  => 'abdulbaset', 'name'  => T_('AbdulBaset AbdulSamad'),],
 
 			// ----------------- afasy
-			['index' => 1020, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'afasy', 'name'  => T_('Mishary Rashid Alafasy'),],
+			['index' => 1020, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'afasy', 'name'  => T_('Mishary Rashid Alafasy'),],
 
 			// ----------------- husary
-			['index' => 1030, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'husary', 'name'  => T_('Mahmoud Khalil Al-Husary'),],
-			['index' => 1031, 'lang' => 'ar', 'type' => T_('Mujawwad'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'husary', 'name'  => T_('Mahmoud Khalil Al-Husary'),],
-			['index' => 1032, 'lang' => 'ar', 'type' => T_('Muallim'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'husary', 'name'  => T_('Mahmoud Khalil Al-Husary'),],
+			['index' => 1030, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'husary', 'name'  => T_('Mahmoud Khalil Al-Husary'),],
+			['index' => 1031, 'lang' => 'ar', 'type' => T_('Mujawwad'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'husary', 'name'  => T_('Mahmoud Khalil Al-Husary'),],
+			['index' => 1032, 'lang' => 'ar', 'type' => T_('Muallim'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'husary', 'name'  => T_('Mahmoud Khalil Al-Husary'),],
 
 			// ----------------- minshawi
-			['index' => 1040, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'minshawi', 'name'  => T_('Mohamed Siddiq al-Minshawi'),],
-			['index' => 1041, 'lang' => 'ar', 'type' => T_('Mujawwad'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'minshawi', 'name'  => T_('Mohamed Siddiq al-Minshawi'),],
+			['index' => 1040, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'minshawi', 'name'  => T_('Mohamed Siddiq al-Minshawi'),],
+			['index' => 1041, 'lang' => 'ar', 'type' => T_('Mujawwad'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'minshawi', 'name'  => T_('Mohamed Siddiq al-Minshawi'),],
 
 			// ----------------- rifai
-			['index' => 1050, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'rifai', 'name'  => T_('Hani ar-Rifai'),],
+			['index' => 1050, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'rifai', 'name'  => T_('Hani ar-Rifai'),],
 
 			// ----------------- shatri
-			['index' => 1060, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'shatri', 'name'  => T_('Abu Bakr al-Shatri'),],
+			['index' => 1060, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'shatri', 'name'  => T_('Abu Bakr al-Shatri'),],
 
 			// ----------------- shuraym
-			['index' => 1070, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'shuraym', 'name'  => T_('Sa`ud ash-Shuraym'),],
+			['index' => 1070, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'shuraym', 'name'  => T_('Sa`ud ash-Shuraym'),],
 
 			// ----------------- sudais
-			['index' => 1080, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'sudais', 'name'  => T_('Abdur-Rahman as-Sudais'),],
+			['index' => 1080, 'lang' => 'ar', 'type' => T_('Murattal'), 'addr'  => 'ayat/alafasy/ayat/Alafasy_128kbps/', 'slug'  => 'sudais', 'name'  => T_('Abdur-Rahman as-Sudais'),],
 
 			// ----------------- trnaslate - az - balayev
-			['index' => 1081, 'lang' => 'az', 'type' => T_('Translate'), 'addr'  => 'translation-az-balayev/', 'slug'  => 'balayev', 'name'  => T_('Rasim Balayev'),],
+			['index' => 1081, 'lang' => 'az', 'type' => T_('Translate'), 'addr'  => 'audio/translation-az-balayev/', 'slug'  => 'balayev', 'name'  => T_('Rasim Balayev'),],
 
 			// ----------------- trnaslate - en - ibrahimwalk
-			['index' => 1082, 'lang' => 'en', 'type' => T_('Translate'), 'addr'  => 'translation-en-sahih-IbrahimWalk/', 'slug'  => 'ibrahimwalk', 'name'  => T_('Ibrahim Walk'),],
+			['index' => 1082, 'lang' => 'en', 'type' => T_('Translate'), 'addr'  => 'audio/translation-en-sahih-IbrahimWalk/', 'slug'  => 'ibrahimwalk', 'name'  => T_('Ibrahim Walk'),],
 
 			// ----------------- trnaslate - fa - fouladvand
-			['index' => 1083, 'lang' => 'en', 'type' => T_('Translate'), 'addr'  => 'translation-en-sahih-foladvand/', 'slug'  => 'fouladvand', 'name'  => T_('Mohammad mahdi fouladvand'),],
+			['index' => 1083, 'lang' => 'en', 'type' => T_('Translate'), 'addr'  => 'audio/translation-en-sahih-foladvand/', 'slug'  => 'fouladvand', 'name'  => T_('Mohammad mahdi fouladvand'),],
 
 			// ----------------- trnaslate - fa - makarem
-			['index' => 1084, 'lang' => 'fa', 'type' => T_('Translate'), 'addr'  => 'translation-fa-kabiri/', 'slug'  => 'makarem', 'name'  => T_('Naser makarem shirazi'),],
-			['index' => 1085, 'lang' => 'fa', 'type' => T_('Translate'), 'addr'  => 'translation-fa-kabiri1/', 'slug'  => 'makarem', 'name'  => T_('Naser makarem shirazi'),],
+			['index' => 1084, 'lang' => 'fa', 'type' => T_('Translate'), 'addr'  => 'audio/translation-fa-kabiri/', 'slug'  => 'makarem', 'name'  => T_('Naser makarem shirazi'),],
+			['index' => 1085, 'lang' => 'fa', 'type' => T_('Translate'), 'addr'  => 'audio/translation-fa-kabiri1/', 'slug'  => 'makarem', 'name'  => T_('Naser makarem shirazi'),],
 
 			// ----------------- trnaslate - fa - qeraati
-			['index' => 1086, 'lang' => 'fa', 'type' => T_('Translate'), 'addr'  => 'translation-fa-qaraati/', 'slug'  => 'qeraati', 'name'  => T_('Mohsen Qeraati'),],
+			['index' => 1086, 'lang' => 'fa', 'type' => T_('Translate'), 'addr'  => 'audio/translation-fa-qaraati/', 'slug'  => 'qaraati', 'name'  => T_('Mohsen Qaraati'),],
 
 		];
 
