a8e22a59372bbd4dcec41ae1fc58db898271f665
Reza <rm.biqarar@gmail.com>
2019-03-14 15:12:09 +0330

    add audio bank db

---
+++ includes/database/upgrade/(db_name)/v.0.2.8_audiobank.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `audiobank` (
+`id` int(10) UNSIGNED NOT NULL auto_increment,
+`qari` varchar(200) NULL DEFAULT NULL,
+`avatar` varchar(2000) NULL DEFAULT NULL,
+`type` varchar(200) NULL DEFAULT NULL,
+`readtype` varchar(200) NULL DEFAULT NULL,
+`filetype` varchar(200) NULL DEFAULT NULL,
+`country` varchar(50) NULL DEFAULT NULL,
+`quality` smallint(4) UNSIGNED NULL DEFAULT NULL,
+`time` int(10) UNSIGNED NULL DEFAULT NULL,
+`size` int(10) UNSIGNED NULL DEFAULT NULL,
+`datecreated` datetime  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
